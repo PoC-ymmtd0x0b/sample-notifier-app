a2d9ec57168df36081737266f3bfb713f0424319
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-03-06 05:18:58 +0900

    日報のCRUDを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :users, only: %i[index show]
+  resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
