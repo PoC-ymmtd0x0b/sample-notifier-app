3bdadb442e77be43367e9ec0488bdd66e02fb017
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-03-06 07:28:32 +0900

    お知らせの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_05_193635) do
+ActiveRecord::Schema.define(version: 2023_03_05_212749) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_announcements_on_user_id"
+  end
 
   create_table "reports", force: :cascade do |t|
     t.string "title", null: false
@@ -34,5 +43,6 @@ ActiveRecord::Schema.define(version: 2023_03_05_193635) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "announcements", "users"
   add_foreign_key "reports", "users"
 end
