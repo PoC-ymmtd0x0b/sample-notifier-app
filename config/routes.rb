816b9b8b9ea1274e4bebcf633f04b2f7ba500a5c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-03-06 04:03:38 +0900

    Userモデルの一覧表示と詳細表示の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#index'
   devise_for :users
+  resources :users, only: %i[index show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
