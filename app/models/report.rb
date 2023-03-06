1b6af5cec9d24161253f87f3073a11ff8c4c7faf
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-03-07 07:37:11 +0900

    最初の日報作成時の通知メールを実装

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,13 @@
 class Report < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+
+  after_save ReportCallbacks.new
+
+  def first?
+    Report.select(:id)
+          .where(user: user)
+          .order(:created_at)
+          .first == self
+  end
 end
