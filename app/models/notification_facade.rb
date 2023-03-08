75b97f2f47ece08568eec17130d9cd50573c819c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-03-08 23:58:49 +0900

    最初の日報作成通知をAbstract Notifier化

--- app/models/notification_facade.rb
+++ app/models/notification_facade.rb
@@ -1,17 +1,15 @@
 class NotificationFacade
   class << self
     def first_report(report, receiver)
+      ActivityNotifier.with(
+        report: report,
+        receiver: receiver
+      ).first_report.notify_now
+
       NotificationMailer.with(
         report: report,
         receiver: receiver
       ).first_report.deliver_later
-
-      Notification.create!(
-        message: "#{report.user.name}さんがはじめての日報を書きました！",
-        user: receiver,
-        sender: report.owner,
-        link: Rails.application.routes.url_helpers.polymorphic_path(report)
-      )
     end
   end
 end
