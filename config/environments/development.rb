9805bfc51726bb8ba7fb41c31396e3873e3b0260
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-04-11 00:14:30 +0900

    ngrokのアクセス許可を設定(普段はコメントアウトで必要な時だけコメントをはずす)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # ngrok
+  # config.hosts << "b38a-69-6-76-165.ngrok-free.app"
 end
